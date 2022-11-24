808225cd053190abeae3be76afe6741189aff012
v <sariyanidi@gmail.com>
2022-11-24 13:00:01 -0500

    updated sample script to visualize landmark variation

--- build/scripts/visualize_landmarks.py
+++ build/scripts/visualize_landmarks.py
@@ -9,7 +9,7 @@ Created on Wed Nov 23 17:31:13 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = np.loadtxt('./exampledata/vid01.landmarks_dexp')
+L = np.loadtxt('../exampleoutput/vid01.landmarks_dexp')
 
 X0 = np.loadtxt('Xl_mean.txt')
 Y0 = np.loadtxt('Yl_mean.txt')
