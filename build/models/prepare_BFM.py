fixed a bug with local basis computation and did some more modifications

build/models/prepare_BFM.py
@@ -111,7 +111,6 @@ def prepare_morphable_model():
 
     p0L_mat = mu3[oi,:]
 
-#%%
     pset_orig = set((np.array(ix_53215).astype(int)+1).tolist())
     old2new0 = {ix_53215[x]: x for x in range(len(ix_53215))}
     old2new_tl0 = {ix_53215[x]+1: x+1 for x in range(len(ix_53215))}
@@ -134,7 +133,6 @@ def prepare_morphable_model():
         for j in range(tl_pre.shape[1]):
             tl_pre[i,j] = old2new_tl0[tl_pre[i,j]]
             
-#%%
 
     pset_pre = set((np.array(ix_23660).astype(int)+1).tolist())
     old2new0 = {ix_23660[x]: x for x in range(len(ix_23660))}
@@ -158,7 +156,6 @@ def prepare_morphable_model():
             tl[i,j] = old2new_tl0[tl[i,j]]
             
 
-#%%
     X0 = mu3[:,0]
     Y0 = mu3[:,1]
     Z0 = mu3[:,2]
@@ -248,9 +245,9 @@ def prepare_morphable_model():
     lis_new = []
     for li in lis:
         lis_new.append(old2new[li])
+        
 
 
-#%%
     ext = 'mm'
     tdir = './MMs/BFM%s-%05d' % (ext, Nnew)
     if not os.path.exists(tdir):
@@ -291,7 +288,7 @@ def prepare_morphable_model():
     np.savetxt('%s/TEX.dat' % tdir, TEXnew)
     np.savetxt('%s/tex_mu.dat' % tdir, tex_mu_new)
     np.savetxt('%s/tl.dat' % tdir, tl_new, fmt='%d')
-
+    np.savetxt('%s/li.dat' % tdir, lis_new, fmt='%d')
     np.savetxt('%s/X0_mean.dat' % tdir, X0new)
     np.savetxt('%s/Y0_mean.dat' % tdir, Y0new)
     np.savetxt('%s/Z0_mean.dat' % tdir, Z0new)

build/scripts/compute_local_exp_coefficients.py
@@ -12,68 +12,54 @@ import warnings
 
 import numpy as np
 import matplotlib.pyplot as plt
+from common import rel_ids, create_expression_sequence
 
-from sklearn.decomposition import DictionaryLearning
 from sklearn.exceptions import ConvergenceWarning
 
 #warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
-canonical_lmks_file = sys.argv[1] 
+exp_coeffs_file = sys.argv[1] 
 local_exp_coeffs_file = sys.argv[2]
 morphable_model = sys.argv[3] #  'BFMmm-19830'
 basis_version = sys.argv[4] # '0.0.1.4'
-
-
 
 sdir = f'models/MMs/{morphable_model}/'
 localized_basis_file = f'models/MMs/{morphable_model}/E/localized_basis/v.{basis_version}.npy'
 basis_set = np.load(localized_basis_file, allow_pickle=True).item()
 
-P = np.loadtxt(canonical_lmks_file)
+# @TODO the code does not work for differential expression computation
+# but only for absolute expressions. It needs to be adapted to the case where
+# basis_set['use_abs'] is set to False!
+assert basis_set['use_abs']
 
-P0 = np.loadtxt(f'{sdir}/p0L_mat.dat')
-X0 = P0[:,0]
-Y0 = P0[:,1]
-Z0 = P0[:,2]
-
-rel_ids   = {'lb': np.array(list(range(0, 5))),
-             'rb': np.array(list(range(5, 10))),
-             'no': np.array(list(range(10, 19))),
-             'le': np.array(list(range(19, 25))),
-             're': np.array(list(range(25, 31))),
-             'ul': np.array(list(range(31, 37))+list(range(43, 47))),
-             'll': np.array(list(range(37, 43))+list(range(47, 51)))}
+li = np.loadtxt(f'{sdir}/li.dat').astype(int)
 
 facial_feats = list(rel_ids.keys())
 
-T = P.shape[0]
+epsilons = np.loadtxt(exp_coeffs_file)
+
+T = epsilons.shape[0]
+
+Es = {}
+
+for feat in rel_ids:
+    rel_id = rel_ids[feat]
+    EX  = np.loadtxt('%s/E/EX_79.dat' % sdir)[li[rel_id],:]
+    EY  = np.loadtxt('%s/E/EY_79.dat' % sdir)[li[rel_id],:]
+    EZ  = np.loadtxt('%s/E/EZ_79.dat' % sdir)[li[rel_id],:]
+    Es[feat] = np.concatenate((EX, EY, EZ), axis=0)
+    
 
 ConvergenceWarning('ignore')
 
 C = []
-for t in range(T):
-    print(f'{t}/{T}')
-    cur = []
-    for feat in facial_feats:
-        rel_id = rel_ids[feat]
+for feat in facial_feats:
+    rel_id = rel_ids[feat]
+    dp = create_expression_sequence(epsilons, Es[feat])
+    coeffs = basis_set[feat].transform(dp).T
+
+    C.append(coeffs)
     
-        p = P[t,:]
-        x = p[::3]
-        y = p[1::3]
-        z = p[2::3]
-        
-        dx = x-X0
-        dy = y-Y0
-        dz = z-Z0
-        
-        # @TODO the following code does not work for differential expression computation
-        # but only for absolute expressions. It needs to be adapted to the case where
-        # basis_set['use_abs'] is set to False!
-        dp = np.concatenate((dx[rel_ids[feat]], dy[rel_ids[feat]], dz[rel_ids[feat]]))
-        coeffs = basis_set[feat].transform(dp.reshape(1,-1)).T
-        cur.append(coeffs)
-    
-    C.append(np.concatenate(cur).reshape(-1,))
-C = np.array(C)
+C = np.concatenate(C)
 
 np.savetxt(local_exp_coeffs_file, C)

build/scripts/learn_localised_bases.py
@@ -11,6 +11,7 @@ import sys
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from common import rel_ids, create_expression_sequence
 
 import cv2
 
@@ -19,13 +20,6 @@ from sklearn.decomposition import DictionaryLearning
 expressions_dir = sys.argv[1] # '/media/v/SSD1TB/dataset/videos/treecam/ML/output/BFMmm-19830.cfg7.global4.curt'
 morphable_model = sys.argv[2] # 'BFMmm-19830
 
-def create_expression_sequence(epsilons, E):
-    ps = []
-    for t in range(epsilons.shape[0]):
-        epsilon = epsilons[t,:]
-        p = ((E @ epsilon)).reshape(-1,1)
-        ps.append(p)
-    return np.array(ps)[:,:,0]
 
 def save_blank_video(T, videpath):
     # Define video properties
@@ -74,6 +68,7 @@ Y0 = np.loadtxt(f'{sdir}/Y0_mean.dat').reshape(-1,1)
 Z0 = np.loadtxt(f'{sdir}/Z0_mean.dat').reshape(-1,1)
 
 tex0 = np.loadtxt(f'{sdir}/tex_mu.dat')
+li = np.loadtxt(f'{sdir}/li.dat').astype(int)
 
 shp = np.concatenate((X0, Y0, Z0), axis=1)
 
@@ -98,22 +93,7 @@ for f in files_all:
 
 files = files[::2]
 
-algorithm = 'cd' 
-
-
-li = [17286,17577,17765,17885,18012,18542,18668,18788,18987,19236,7882,7896,7905,7911,6479,7323,
-      7922,8523,9362,1586,3480,4770,5807,4266,3236, 10176,11203,12364,14269,12636,11602,5243,5875,
-      7096,7936,9016,10244,10644,9638,8796,7956,7116,6269,5629,6985,7945,8905,10386,8669,7949,7229]
-
-li = np.array(li)
-
-rel_ids  =  {'lb': np.array(list(range(0, 5))),
-             'rb': np.array(list(range(5, 10))),
-             'no': np.array(list(range(10, 19))),
-             'le': np.array(list(range(19, 25))),
-             're': np.array(list(range(25, 31))),
-             'ul': np.array(list(range(31, 37))+list(range(43, 47))),
-             'll': np.array(list(range(37, 43))+list(range(47, 51)))}
+algorithm = 'cd'
 
 num_comps = {'lb': 3+2,
              'rb': 3+2,

build/scripts/produce_canonicalized_3Dlandmarks.py
@@ -12,13 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def create_expression_sequence(epsilons, E):
-    ps = []
-    for t in range(epsilons.shape[0]):
-        epsilon = epsilons[t,:]
-        p = ((E @ epsilon)).reshape(-1,1)
-        ps.append(p)
-    return np.array(ps)[:,:,0]
+from common import create_expression_sequence
 
 assert len(sys.argv) >= 4, """At least three arguments are needed to produce canonicalized landmarks: 
     #expression_path #landmark_path #morphable_model_name"""
@@ -38,11 +32,7 @@ if len(sys.argv) >= 5:
 
 assert os.path.exists(epath), 'Expression file does not exist!'
 
-li = [17286,17577,17765,17885,18012,18542,18668,18788,18987,19236,7882,7896,7905,7911,6479,7323,
-      7922,8523,9362,1586,3480,4770,5807,4266,3236, 10176,11203,12364,14269,12636,11602,5243,5875,
-      7096,7936,9016,10244,10644,9638,8796,7956,7116,6269,5629,6985,7945,8905,10386,8669,7949,7229]
-
-li = np.array(li)
+li = np.loadtxt(f'{sdir}/li.dat').astype(int)
 
 X0 = np.loadtxt(f'{sdir}/X0_mean.dat').reshape(-1,1)[li]
 Y0 = np.loadtxt(f'{sdir}/Y0_mean.dat').reshape(-1,1)[li]

build/scripts/total_variance_rec.py
@@ -27,14 +27,13 @@ for i in range(e1.shape[1]):
     e1[:,i:i+1] = imputer.fit_transform(e1[:,i:i+1])
 
 
-li = [17286,17577,17765,17885,18012,18542,18668,18788,18987,19236,7882,7896,7905,7911,6479,7323,
-      7922,8523,9362,1586,3480,4770,5807,4266,3236, 10176,11203,12364,14269,12636,11602,5243,5875,
-      7096,7936,9016,10244,10644,9638,8796,7956,7116,6269,5629,6985,7945,8905,10386,8669,7949,7229]
-
 T = e1.shape[0]
 K = e1.shape[1]
 
 sdir = './models/MMs/%s' % morphable_model 
+li = np.loadtxt(f'{sdir}/li.dat').astype(int)
+
+
 EX  = np.loadtxt('%s/E/EX_79.dat' % sdir)[li,:]
 EY  = np.loadtxt('%s/E/EY_79.dat' % sdir)[li,:]
 EZ  = np.loadtxt('%s/E/EZ_79.dat' % sdir)[li,:]