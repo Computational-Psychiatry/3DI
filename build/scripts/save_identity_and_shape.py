5e1d3f8678ded9dc69a7f94c6e1f633ef592c009
Birkan Tunc <TUNCBIR@GMAIL.COM>
2024-12-13 15:26:02 -0500

    Update save_identity_and_shape.py

--- build/scripts/save_identity_and_shape.py
+++ build/scripts/save_identity_and_shape.py
@@ -6,14 +6,34 @@ Created on Fri Dec  13 15:20:05 2024
 @author: b
 """
 import numpy as np
-import cvxpy as cp
-from time import time
-from sklearn.impute import KNNImputer
-
 import sys
 
-# bpath = sys.argv[1]
-# ppath = sys.argv[2]
-ppath = sys.argv[1]
-pnewpath = sys.argv[2]
+alpha = sys.argv[1]
+beta = sys.argv[2]
+shp_path = sys.argv[3]
+tex_path = sys.argv[4]
+morphable_model = sys.argv[5]
+
+sdir = './models/MMs/%s' % morphable_model 
+
+IX  = np.loadtxt('%s/IX.dat' % sdir)
+IY  = np.loadtxt('%s/IY.dat' % sdir)
+IZ  = np.loadtxt('%s/IZ.dat' % sdir)
+
+TEX  = np.loadtxt('%s/TEX.dat' % sdir)
+
+tex_mu = np.loadtxt('%s/tex_mu.dat' % sdir)
+
+x0 = np.loadtxt('%s/X0_mean.dat' % sdir)
+y0 = np.loadtxt('%s/Y0_mean.dat' % sdir)
+z0 = np.loadtxt('%s/Z0_mean.dat' % sdir)
+
+x = (x0+(IX @ alpha)).reshape(-1,1)
+y = (y0+(IY @ alpha)).reshape(-1,1)
+z = (z0+(IZ @ alpha)).reshape(-1,1)
+
+tex = (tex_mu+(TEX @ beta)).reshape(-1, 1)
+
+np.savetxt(shp_path, np.concatenate((x,y,z), axis=1))
+np.savetxt(tex_path, tex)
 
