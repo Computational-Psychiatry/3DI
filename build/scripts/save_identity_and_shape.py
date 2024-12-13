f920a93b1ab6b901b12834507990e23878c2c292
Birkan Tunc <TUNCBIR@GMAIL.COM>
2024-12-13 15:20:39 -0500

    Update save_identity_and_shape.py

--- build/scripts/save_identity_and_shape.py
+++ build/scripts/save_identity_and_shape.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  13 15:20:05 2024
+
+@author: b
+"""
+import numpy as np
+import cvxpy as cp
+from time import time
+from sklearn.impute import KNNImputer
+
+import sys
+
+# bpath = sys.argv[1]
+# ppath = sys.argv[2]
+ppath = sys.argv[1]
+pnewpath = sys.argv[2]
 
